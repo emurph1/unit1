95ef37009f845e6a0cdb4effb500d9326f1e31fd
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-08-31 14:48:41 -0400

    Updated from Brython Server: 8/31/2017 2:48:42 PM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,12 @@
+#Emily Murphy
+#2017-08-31
+#nameAge.py - user asks questions
 
+name = input('What is your first and last name?')
+print(name)
+name1, name2 = name.split()
+age = int(input('How old are you?'))
+print(age)
+print(len(name1))
+print(len(name2))
+print('Next year you will be', age+1, 'years old')
