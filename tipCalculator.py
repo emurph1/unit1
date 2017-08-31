50c6a705a4ccee70fae18b727e24a66d23c2481a
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-08-31 14:53:48 -0400

    Updated from Brython Server: 8/31/2017 2:53:50 PM

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,1 +1,9 @@
+#Emily Murphy
+#2017-08-31
+#tipCalculator.py - finding out tip
 
+meal = (input('How much was the meal?'))
+print(meal)
+tip = int(input('How much to tip?'))
+print(tip)
+print('You should tip', meal*tip)
