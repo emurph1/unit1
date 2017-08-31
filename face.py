520fd2a3243f2624bf0b2f7d588484c6f43642dc
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-08-31 14:29:34 -0400

    Updated from Brython Server: 8/31/2017 2:29:35 PM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Emily Murphy
+#2017-08-31
+#face.py - make a face
 
+print('     xxxxx')
+print('   x       x')
+print('((    o o    ))')
+print('   |   v   |')
+print('   |  ===  |')
+print('     -----')
