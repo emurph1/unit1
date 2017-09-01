ec0d72083f1238d09313ed78c87bd257a7568905
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-01 14:49:44 -0400

    Updated from Brython Server: 9/1/2017 2:49:45 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,9 @@
+#Emily Murphy
+#2017-09-01
+#stringAnalysis.py - takes sentence as input and reports how many words and cahracters the sentence has
 
+s1 = input('Enter sentence: ')
+charc = input('Enter a character to search for: ')
+print('Your sentence has ', s1.count(charc), 'of the character', charc)
+word = input('Enter a word to search for: ')
+print('Your sentence has ', s1.count(word), 'of the word', word)
