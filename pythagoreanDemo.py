392b50a6b87dfa544bbf72193d1639ed25216947
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-01 14:26:25 -0400

    Updated from Brython Server: 9/1/2017 2:26:26 PM

--- pythagoreanDemo.py
+++ pythagoreanDemo.py
@@ -1,1 +1,11 @@
+#Emily Murphy  
+#2017-09-01
+#pythagoreanDemo.py - how to find hypotenuse with the other two leg lengths (right triangle)
+
+from math import sqrt
+
+a = float(input('Enter leg 1: '))
+# : because not a question
+b = float(input('Enter leg 2: '))
+print(sqrt(a**2 + b**2))
 
