5c17a75a09abcdaeb89d8f6392f09cc361e7cad9
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-01 13:22:43 -0400

    Updated from Brython Server: 9/1/2017 1:22:44 PM

--- km2miles.py
+++ km2miles.py
@@ -2,6 +2,9 @@
 #2017-08-31
 #km2miles.py - converting km to miles
 
-input('How many kilometers? ')
-print(' 5.0 km is 3.107 miles')
+km = int(input('How many kilometers? '))
+conv_fac = 0.621371
+miles = round((km * conv_fac), 3)
+
+print(km, 'kilometers is', miles, 'miles')
 
