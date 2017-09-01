f84bcb60d9761024c43ece06213e70f99ae9a20d
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-01 14:27:54 -0400

    Updated from Brython Server: 9/1/2017 2:27:54 PM

--- pythagoreanDemo.py
+++ pythagoreanDemo.py
@@ -7,5 +7,5 @@ from math import sqrt
 a = float(input('Enter leg 1: '))
 # : because not a question
 b = float(input('Enter leg 2: '))
-print(sqrt(a**2 + b**2))
+print('The hypotenuse is', sqrt(a**2 + b**2))
 
