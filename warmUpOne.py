4d5b81b819b5aaa8111cd5f1d5cc54131999236b
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-06 13:39:20 -0400

    Updated from Brython Server: 9/6/2017 1:39:26 PM

--- warmUpOne.py
+++ warmUpOne.py
@@ -1,1 +1,8 @@
+#Emily Murphy
+#2017-09-06
+#warmUpOne.py - find the area of a triangle
 
+base = float(input('Enter the length of the base: '))
+height = float(input('Enter the height of triangle: '))
+area = int((base * height)/2)
+print('The area of the triangle is', area)
