8fe3980df3e11538228865b3d0c1a3d274609d57
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-07 14:52:11 -0400

    Updated from Brython Server: 9/7/2017 2:52:12 PM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,11 @@
+#Emily Murphy
+#2017-09-06
+#isItATriangle.py - input three lengths and determine if they can make a triangle
 
+sideA = float(input('Enter side A: '))
+sideB = float(input('Enter side B: '))
+sideC = float(input('Enter side C: '))
+float(max(sideA,sideB, sideC))
+float(min(sideA,sideB, sideC))
+middle = float((max(sideA,sideB, sideC)- min(sideA,sideB, sideC)))
+print((float(min(sideA,sideB, sideC)) + middle)>float(max(sideA,sideB, sideC)))
