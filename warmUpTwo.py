0a19cf64e257573c85a5ef028a1ec8d05c5729d4
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-07 14:20:09 -0400

    Updated from Brython Server: 9/7/2017 2:20:09 PM

--- warmUpTwo.py
+++ warmUpTwo.py
@@ -1,1 +1,9 @@
+#Emily Murphy
+#2017-09-07
+#warmUpTwo.py - warm up two
 
+from random import randint
+
+name = input('Enter your name: ')
+num1 = randint(1, 100)
+print('Your lucky number is', num1)
