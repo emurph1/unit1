1325086597cfc7edcb9b26f9a4aeffeac685f3f3
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-11 14:17:54 -0400

    Updated from Brython Server: 9/11/2017 2:17:54 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,11 @@
+#Emily Murphy
+#2017-09-11
+#quiz1.py
+
+print('Emily Murphy')
+num1 = float(input('Enter first number: '))
+num2 = float(input('Enter second number: '))
+print('The sum is', (num1 + num2))
+print('The product is', (num1 * num2))
+print('Your lucky number is', ((num1 * 7) + (num2 * 7)))
 
